e0c4444cdf7e2ff6677b882b773943952e622155
Madeleine Dormeyer <mdormeye@smail.uni-koeln.de>
2022-12-23 13:09:47 +0100

    change

--- main/Main.java
+++ main/Main.java
@@ -5,8 +5,7 @@ public class Main {
 	public static void main(String [] args) {
 		System.out.println("Code at line 6");
 		System.out.println("Code at line 7 in main branch");
-		
-		System.out.println("Changes for superlinter");
+		System.out.println("Changes");
 	}
 
 }
