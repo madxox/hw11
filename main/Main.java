c84b1444c922b6e4704811f6758d48c6476d6ab9
Madeleine Dormeyer <mdormeye@smail.uni-koeln.de>
2022-12-23 12:51:02 +0100

    refactor

--- main/Main.java
+++ main/Main.java
@@ -1,11 +1,12 @@
-package hw11_testrepo;
+package main;
 
 public class Main {
 	
 	public static void main(String [] args) {
 		System.out.println("Code at line 6");
 		System.out.println("Code at line 7 in main branch");
-		System.out.println("Changes for superlinter!");
+		
+		System.out.println("Changes for superlinter");
 	}
 
 }
